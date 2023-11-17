342a4b9fc02549dde073ccc4a6442d42777f25d0
hsin yu <tina2793778@gmail.com>
2023-11-17 18:48:54 +0800

    feat: 🎸 新增攝影師卡片切版

---
+++ app/components/PhotographerCard.js
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import Image from 'next/image';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+// import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
+export default function PhotographerCard({
+  name,
+  location,
+  rate,
+  catagories,
+  price,
+}) {
+  return (
+    <Card
+      sx={{
+        position: 'relative',
+      }}
+    >
+      <Image
+        alt="Random image"
+        src="/photographer-card-1.png"
+        width={319}
+        height={424}
+        style={{
+          width: '100%',
+          height: 'auto',
+          aspectRatio: '3 / 4',
+          objectFit: 'cover',
+          display: 'block',
+        }}
+        priority={true}
+      />
+      <CardContent
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Typography variant="body2" color="text.secondary">
+          {location}
+        </Typography>
+        <Box>
+          <Typography
+            gutterBottom
+            variant="h3"
+            component="h3"
+            sx={{
+              fontSize: 18,
+            }}
+          >
+            {name} <Chip label={rate} />
+          </Typography>
+          <Stack direction="row" spacing={1} flexWrap="wrap">
+            {catagories.map((item, index) => (
+              <Chip
+                label={item}
+                key={index + item}
+                sx={{
+                  fontSize: 14,
+                }}
+              />
+            ))}
+          </Stack>
+          <Typography variant="body2" color="text.secondary">
+            {price}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,67 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Container from '@mui/material/Container';
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import PhotographerCard from '@/app/components/PhotographerCard';
 
-export default function Home() {
+const photographerData = [
+  {
+    name: '周周',
+    location: '台北',
+    rate: 5,
+    catagories: ['商業攝影', '旅遊拍攝', '風格街拍'],
+    price: 2000,
+    id: 1,
+  },
+  {
+    name: '周周',
+    location: '台北',
+    rate: 5,
+    catagories: ['商業攝影', '旅遊拍攝', '風格街拍'],
+    price: 2000,
+    id: 2,
+  },
+  {
+    name: '周周',
+    location: '台北',
+    rate: 5,
+    catagories: ['商業攝影', '旅遊拍攝', '風格街拍'],
+    price: 2000,
+    id: 3,
+  },
+  {
+    name: '周周',
+    location: '台北',
+    rate: 5,
+    catagories: ['商業攝影', '旅遊拍攝', '風格街拍'],
+    price: 2000,
+    id: 4,
+  },
+];
+
+export default function HomePage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <Container component="section">
+      <Grid container rowSpacing={3} columnSpacing={3} component="ul">
+        {photographerData.map((card) => (
+          <Grid sm={6} md={3} key={card.id} sx={{
+            margin: '0 auto'
+          }} component="li">
+            <PhotographerCard 
+              name={card.name}
+              location={card.location}
+              rate={card.rate}
+              catagories={card.catagories}
+              price={card.price} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
